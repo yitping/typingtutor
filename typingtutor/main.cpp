e7bc0a1ed1343a283ccea7cd533afdb98eefa43e
Yitping Kok <yitping@mpe.mpg.de>
2015-03-06 08:10:46 +0800

    - reference line refreshes after CR/LF - color coded print out based on error and character (erroneous space is printed as checkerboard) - no backspace allowed

--- typingtutor/main.cpp
+++ typingtutor/main.cpp
@@ -6,52 +6,88 @@
 //  Copyright (c) 2015 Onslow143. All rights reserved.
 //
 
-#include <iostream>
-#include <string>
-#include <ncurses.h>
-#include <time.h>
+#include "main.h"
 
-int scrW, scrH;
+int screenW, screenH;
 int buffersize;
-int n_char = 0;
-char buffer[60] = { 0 };
-std::string strTyped, strMaster;
+std::string charsTyped, charsRefer;
+int color1, color2;
 
-void updateScreen()
+int actionLineY, actionLineX1, actionLineX2;
+
+void setupScreen()
 {
-	int m, n;
-	m = scrH*4/10;
-	n = (scrW - buffersize)/2;
-	mvprintw(m-3, n, "Jiayu's Typing Tutor");
-	mvprintw(m-1, n, strMaster.c_str());
+	actionLineY  = screenH*4/10;
+	actionLineX1 = (screenW - buffersize)/2;
+	actionLineX2 = actionLineX1 + buffersize - 1;
+	attron(A_UNDERLINE);
+	mvprintw(actionLineY-3, actionLineX1, TITLE_NAME);
+	attroff(A_UNDERLINE);
+}
+
+void updateReferLine(std::string &s)
+{
+	attron(COLOR_PAIR(COLOR_ID_DEFAULT));
+	mvprintw(actionLineY-1, actionLineX1, s.c_str());
+}
+
+void updateActionLine(std::string &s, std::string &r)
+{
+	if (s.size() == 0)
+	{
+		move(actionLineY, actionLineX1);
+		return;
+	}
+	
+	int i = (int) s.size() - 1;
+	bool err = FALSE;
+	if (s[i] != r[i]) err = TRUE;
+	// TODO: if last character in buffer is a space, ignore?
+	
 	attron(A_BOLD);
-	mvprintw(m, n, strTyped.c_str());
+	if (err) attron(COLOR_PAIR(COLOR_ID_ERROR));
+	else attron(COLOR_PAIR(COLOR_ID_DEFAULT));
+	
+	if ((s[i] == ASCII_SPACE) && err)
+	{
+		mvaddch(actionLineY, actionLineX1+i, ACS_CKBOARD);
+		//mvaddch(actionLineY, actionLineX1+i, s[i] | A_ALTCHARSET);
+	}
+	else
+	{
+		mvaddch(actionLineY, actionLineX1+i, s[i]);
+	}
+	
 	attroff(A_BOLD);
+}
+
+void clearActionLine()
+{
+	std::string blank(buffersize, ' ');
+	mvprintw(actionLineY, actionLineX1, blank.c_str());
+	move(actionLineY, actionLineX1);
+}
+
+bool isEOL(std::string &a)
+{
+	return (a.size() < buffersize) ? FALSE : TRUE;
 }
 
 void clearBuffer(std::string &a)
 {
 	a.clear();
-	n_char = 0;
-	a.assign("");
 }
 
-void addChar(std::string &a, char c)
+int addChar(std::string &a, char c)
 {
-	if (n_char > buffersize-1) clearBuffer(a);
-	a[n_char]   = c;
-	a[n_char+1] = '\0';
-	n_char += 1;
+	// a.apacity() includes space for the NULL char
+	if (a.size() == buffersize) return 0;
+	a += c;
+	return 1;
+	// TODO: add timing feature
 }
 
-void delChar(std::string &a)
-{
-	a[n_char-1] = '\0';
-	n_char -= 1;
-	if (n_char < 0) n_char = 0;
-}
-
-std::string genStr(char *a, int n, int len)
+std::string generateString(char *a, int n, int len)
 {
 	std::string s;
 	int i, j;
@@ -69,57 +105,109 @@ std::string genStr(char *a, int n, int len)
 	return s;
 }
 
+bool isPrintableKey(char c)
+{
+	if ((c >= 32) && (c <=127)) return TRUE;
+	else return FALSE;
+}
+
+bool isSpecialKey(char c)
+{
+	switch (c)
+	{
+		case ASCII_BACKSPACE:
+		case ASCII_DELETE:
+		case ASCII_CRETURN:
+		case ASCII_NEWLINE:
+			return TRUE;
+		default:
+			return FALSE;
+	}
+}
+
 int main(int argc, const char * argv[])
 {
 	int ch;
 	
 	// initialize screen
 	initscr();
-	getmaxyx(stdscr, scrH, scrW);
+	if ((has_colors() == FALSE) && (can_change_color() == FALSE))
+	{
+		endwin();
+		std::cout << "Your terminal does not support color" << std::endl;
+		exit(1);
+	}
+	start_color();
+	use_default_colors();
+	init_pair(COLOR_ID_ERROR, COLOR_MAGENTA, -1);
+	init_pair(COLOR_ID_DEFAULT, -1, -1);
+	
+	getmaxyx(stdscr, screenH, screenW);
 	raw();
 	keypad(stdscr, TRUE);
 	noecho();
 	
 	// buffersize is odd if screen width is odd and vice versa
-	buffersize  = scrW*3/4;
-	buffersize += (scrW % 2) ? ((buffersize % 2) + 1) : (buffersize % 2);
+	buffersize  = screenW*3/4;
+	buffersize += (screenW % 2) ? ((buffersize % 2) + 1) : (buffersize % 2);
 
 	srand((unsigned int)time(NULL));
 	char a[] = {'a', 's', 'd', 'f', ' '};
-	strMaster = genStr(a, 5, buffersize);
-	strTyped.resize(buffersize, '\0');
+	charsRefer = generateString(a, 5, buffersize);
+	charsTyped.resize(buffersize);
+	charsTyped.clear();
 	
-	updateScreen();
+	setupScreen();
+	updateReferLine(charsRefer);
+	updateActionLine(charsTyped, charsRefer);
 	
 	ch = getch();
-	while (ch != 3)
+	while (ch != ASCII_CTRL_C)
 	{
+		if (!isPrintableKey(ch) && !isSpecialKey(ch))
+		{
+			ch = getch();
+			continue;
+		}
 		switch (ch)
 		{
-			case 8:
-			case 127:
-				delChar(strTyped);
+			case ASCII_BACKSPACE:
+			case ASCII_DELETE:
+				std::cout << (char) ASCII_CTRL_G; // sound the bell
+				refresh();
 				break;
-			case 10:
-			case 13:
-				clearBuffer(strTyped);
-				break;
-			case KEY_UP:
-			case KEY_DOWN:
-			case KEY_RIGHT:
-			case KEY_LEFT:
+			case ASCII_NEWLINE:
+			case ASCII_CRETURN:
+				if (isEOL(charsTyped))
+				{
+					charsRefer = generateString(a, 5, buffersize);
+					updateReferLine(charsRefer);
+					clearActionLine();
+					clearBuffer(charsTyped);
+				}
 				break;
 			default:
-				addChar(strTyped, ch);
+				if (addChar(charsTyped, ch))
+				{
+					updateActionLine(charsTyped, charsRefer);
+				}
+				// if at end of the line
+				// - refresh reference string
+				// - updateReferLine()
+				// - move to actionLineX1
 		}
-		updateScreen();
 		ch = getch();
 	}
 	
 	refresh();
 	endwin();
 	
-	std::cout << "buffersize = " << buffersize << std::endl;
+	/*
+	std::cout << "buffersize= " << buffersize << std::endl;
+	std::cout << "reference = " << charsRefer.size() << std::endl;
+	std::cout << "capacity  = " << charsTyped.capacity() << std::endl;
+	std::cout << "typed_in  = " << charsTyped.size() << std::endl;
+	 */
 	printf("Goodbye!\n");
 	return 0;
 }
---
+++ typingtutor/main.h
@@ -0,0 +1,32 @@
+//
+//  main.h
+//  typingtutor
+//
+//  Created by Yitping Kok on 4/03/2015.
+//  Copyright (c) 2015 Onslow143. All rights reserved.
+//
+
+#ifndef typingtutor_main_h
+#define typingtutor_main_h
+
+#include <iostream>
+#include <string>
+#include <ncurses.h>
+#include <time.h>
+
+#define COLOR_ID_DEFAULT 1
+#define COLOR_ID_ERROR 2
+
+#define TITLE_NAME "Jiayu's Typing Tutor"
+
+#define ASCII_CTRL_C 3
+#define ASCII_CTRL_G 7 // bell
+#define ASCII_BACKSPACE 8
+#define ASCII_NEWLINE 10
+#define ASCII_CRETURN 13
+#define ASCII_SPACE 32
+#define ASCII_SOLID 32
+#define ASCII_DELETE 127
+#define ASCII_BLOCK 219
+
+#endif
