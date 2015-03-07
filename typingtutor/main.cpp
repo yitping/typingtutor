ff079894436e5daff6c4741f2d4e8639f4046b44
Yitping Kok <yitping@mpe.mpg.de>
2015-03-07 21:38:06 +0800

    - has command line options - statistics on typing speed and accuracy

--- typingtutor/main.cpp
+++ typingtutor/main.cpp
@@ -12,14 +12,51 @@ int screenW, screenH;
 int buffersize;
 std::string charsTyped, charsRefer;
 int color1, color2;
+int totalLetter, totalError;
+float totalTime, letterPerMin, errorPerLetter;
+std::time_t ttTimer;
+bool isLastLetterAccurate;
 
 int actionLineY, actionLineX1, actionLineX2;
+int infoLineY, infoLineX1, infoLineX2;
+
+void initTT()
+{
+	// initialize screen
+	initscr();
+	if ((has_colors() == FALSE) && (can_change_color() == FALSE))
+	{
+		endwin();
+		std::cout << "Your terminal does not support color" << std::endl;
+		exit(1);
+	}
+	start_color();
+	use_default_colors();
+	init_pair(COLOR_ID_ERROR, COLOR_MAGENTA, -1);
+	init_pair(COLOR_ID_DEFAULT, -1, -1);
+	
+	getmaxyx(stdscr, screenH, screenW);
+	raw();
+	keypad(stdscr, TRUE);
+	noecho();
+	
+	// buffersize is odd if screen width is odd and vice versa
+	buffersize  = screenW*3/4;
+	buffersize += (screenW % 2) ? ((buffersize % 2) + 1) : (buffersize % 2);
+	
+	resetStatistics();
+
+	srand((unsigned int)time(NULL));
+}
 
 void setupScreen()
 {
 	actionLineY  = screenH*4/10;
 	actionLineX1 = (screenW - buffersize)/2;
 	actionLineX2 = actionLineX1 + buffersize - 1;
+	infoLineY  = screenH-1;
+	infoLineX1 = actionLineX1;
+	infoLineX2 = actionLineX2;
 	attron(A_UNDERLINE);
 	mvprintw(actionLineY-3, actionLineX1, TITLE_NAME);
 	attroff(A_UNDERLINE);
@@ -48,10 +85,11 @@ void updateActionLine(std::string &s, std::string &r)
 	if (err) attron(COLOR_PAIR(COLOR_ID_ERROR));
 	else attron(COLOR_PAIR(COLOR_ID_DEFAULT));
 	
+	if (err) totalError += 1;
+	
 	if ((s[i] == ASCII_SPACE) && err)
 	{
 		mvaddch(actionLineY, actionLineX1+i, ACS_CKBOARD);
-		//mvaddch(actionLineY, actionLineX1+i, s[i] | A_ALTCHARSET);
 	}
 	else
 	{
@@ -68,6 +106,35 @@ void clearActionLine()
 	move(actionLineY, actionLineX1);
 }
 
+void updateInfoLine()
+{
+	char info[100];
+	computeStatistics();
+	/*
+	if (errorPerLetter > 0.99)
+	{
+		sprintf(info, "L/min = %3.0f | Mistakes/100L = XX", letterPerMin);
+	}
+	else
+	{
+		sprintf(info, "L/min = %3.0f | Mistakes/100L = %2.0f%%", letterPerMin, errorPerLetter*100);
+	}
+	 */
+	sprintf(info, "L/min = %3.0f | Mistakes %s %2.0f%%%%",
+					letterPerMin,
+					(errorPerLetter > 0.99) ? ">" : "=",
+					((errorPerLetter > 0.99) ? 0.99 : errorPerLetter)*100);
+	//sprintf(info, "tL = %3d | tE = %3d | tT = %4.2f | L/T = %.4f | E/L = %2.0f",
+	//				totalLetter, totalError, totalTime, letterPerMin, errorPerLetter*100);
+	attron(COLOR_PAIR(COLOR_ID_DEFAULT));
+	mvprintw(infoLineY, infoLineX1, info);
+}
+
+void updateCursorPosition(std::string &a)
+{
+	move(actionLineY, actionLineX1+(int)a.size());
+}
+
 bool isEOL(std::string &a)
 {
 	return (a.size() < buffersize) ? FALSE : TRUE;
@@ -78,13 +145,35 @@ void clearBuffer(std::string &a)
 	a.clear();
 }
 
+void resetStatistics()
+{
+	totalLetter = 0;
+	totalError  = 0;
+	totalTime      = 0.0;
+	letterPerMin   = 0.0;
+	errorPerLetter = 0.0;
+}
+
+void computeStatistics()
+{
+	letterPerMin   = (totalTime > 0) ? (totalLetter-totalError)/(float)totalTime*60 : 0.0;
+	errorPerLetter = (totalLetter > 0) ? (float)totalError/totalLetter : 0.0;
+}
+
 int addChar(std::string &a, char c)
 {
-	// a.apacity() includes space for the NULL char
-	if (a.size() == buffersize) return 0;
+	// a.apacity() includes extra spaces, e.g. for the NULL char
+	if (a.size() == buffersize)
+	{
+		return 0;
+	}
+	if ((ttTimer == (time_t)-1) || (a.size() == 0)) ttTimer = std::time(NULL);
+	std::time_t temp = std::time(NULL);
+	totalTime += difftime(temp,ttTimer);
+	ttTimer = temp;
+	totalLetter += 1;
 	a += c;
 	return 1;
-	// TODO: add timing feature
 }
 
 std::string generateString(char *a, int n, int len)
@@ -115,6 +204,7 @@ bool isSpecialKey(char c)
 {
 	switch (c)
 	{
+		case ASCII_CTRL_R:
 		case ASCII_BACKSPACE:
 		case ASCII_DELETE:
 		case ASCII_CRETURN:
@@ -125,41 +215,102 @@ bool isSpecialKey(char c)
 	}
 }
 
-int main(int argc, const char * argv[])
+void printCLHelp(char *clarg0)
 {
-	int ch;
-	
-	// initialize screen
-	initscr();
-	if ((has_colors() == FALSE) && (can_change_color() == FALSE))
-	{
-		endwin();
-		std::cout << "Your terminal does not support color" << std::endl;
-		exit(1);
-	}
-	start_color();
-	use_default_colors();
-	init_pair(COLOR_ID_ERROR, COLOR_MAGENTA, -1);
-	init_pair(COLOR_ID_DEFAULT, -1, -1);
-	
-	getmaxyx(stdscr, screenH, screenW);
-	raw();
-	keypad(stdscr, TRUE);
-	noecho();
-	
-	// buffersize is odd if screen width is odd and vice versa
-	buffersize  = screenW*3/4;
-	buffersize += (screenW % 2) ? ((buffersize % 2) + 1) : (buffersize % 2);
+	std::cout << "Usage: " << clarg0 << " ";
+	std::cout << "-c \"<characters to train in delimited format (default=csv)>\" ";
+	std::cout << "[-s <separator>]";
+	std::cout << "[-h]";
+	std::cout << std::endl;
+}
 
-	srand((unsigned int)time(NULL));
-	char a[] = {'a', 's', 'd', 'f', ' '};
-	charsRefer = generateString(a, 5, buffersize);
+bool hasChar(std::vector<char> &a, char c)
+{
+	for (int i=0; i<a.size(); i++)
+	{
+		if (a[i] == c) return TRUE;
+	}
+	return FALSE;
+}
+
+int splitStringToChar(char *in, std::vector<char> &out, char sep)
+{
+	char *p;
+	p = strtok(in, &sep);
+	while (p != NULL)
+	{
+		if (strlen(p) > 1)
+		{
+			std::cerr << "error: only one character per field" << std::endl;
+			return 1;
+		}
+		if (!hasChar(out,*p))
+		{
+			out.push_back(*p);
+		}
+		p = strtok(NULL, &sep);
+	}
+	std::cout << "size of out = " << out.size() << std::endl;
+	std::cout << "out = " << out.data() << std::endl;
+	out.push_back(' ');
+	return 0;
+}
+
+int main(int argc, char * argv[])
+{
+	int ch, opt;
+	int errflag;
+	char *optargTrainingSet=NULL, optargSep=',';
+	std::vector<char> trainingSet;
+	
+	while ((opt = getopt(argc, argv, ":hc:s:")) != -1)
+	{
+		switch (opt)
+		{
+			case 'h':
+				errflag++;
+				break;
+			case 'c':
+				optargTrainingSet = optarg;
+				break;
+			case 's':
+				sscanf(optarg, "%c", &optargSep);
+				break;
+			case ':':
+				std::cerr << "option -" << optopt << "requires an operand" << std::endl;
+				errflag++;
+				break;
+			case '?':
+				std::cerr << "unrecognized option:-" << optopt << std::endl;
+				errflag++;
+				break;
+		}
+	}
+	
+	if (errflag || (optargTrainingSet == NULL))
+	{
+		printCLHelp(argv[0]);
+		return EXIT_FAILURE;
+	}
+	
+	if (splitStringToChar(optargTrainingSet, trainingSet, optargSep) != 0)
+	{
+		return EXIT_FAILURE;
+	}
+	
+	// Initialize app
+	initTT();
+	
+	//char a[] = {'a', 's', 'd', 'f', ' '};
+	charsRefer = generateString(trainingSet.data(), (int)trainingSet.size(), buffersize);
 	charsTyped.resize(buffersize);
 	charsTyped.clear();
 	
 	setupScreen();
 	updateReferLine(charsRefer);
 	updateActionLine(charsTyped, charsRefer);
+	updateInfoLine();
+	updateCursorPosition(charsTyped);
 	
 	ch = getch();
 	while (ch != ASCII_CTRL_C)
@@ -176,11 +327,13 @@ int main(int argc, const char * argv[])
 				std::cout << (char) ASCII_CTRL_G; // sound the bell
 				refresh();
 				break;
+			case ASCII_CTRL_R:
+				resetStatistics();
 			case ASCII_NEWLINE:
 			case ASCII_CRETURN:
-				if (isEOL(charsTyped))
+				if (isEOL(charsTyped) || (ch == ASCII_CTRL_R))
 				{
-					charsRefer = generateString(a, 5, buffersize);
+					charsRefer = generateString(trainingSet.data(), (int)trainingSet.size(), buffersize);
 					updateReferLine(charsRefer);
 					clearActionLine();
 					clearBuffer(charsTyped);
@@ -191,10 +344,8 @@ int main(int argc, const char * argv[])
 				{
 					updateActionLine(charsTyped, charsRefer);
 				}
-				// if at end of the line
-				// - refresh reference string
-				// - updateReferLine()
-				// - move to actionLineX1
+				updateInfoLine();
+				updateCursorPosition(charsTyped);
 		}
 		ch = getch();
 	}
--- typingtutor/main.h
+++ typingtutor/main.h
@@ -11,16 +11,21 @@
 
 #include <iostream>
 #include <string>
+#include <iomanip>
+#include <vector>
 #include <ncurses.h>
 #include <time.h>
+#include <unistd.h>
 
 #define COLOR_ID_DEFAULT 1
 #define COLOR_ID_ERROR 2
 
 #define TITLE_NAME "Jiayu's Typing Tutor"
+#define MIN_SCREEN_WIDTH 50
 
 #define ASCII_CTRL_C 3
 #define ASCII_CTRL_G 7 // bell
+#define ASCII_CTRL_R 18
 #define ASCII_BACKSPACE 8
 #define ASCII_NEWLINE 10
 #define ASCII_CRETURN 13
@@ -28,5 +33,9 @@
 #define ASCII_SOLID 32
 #define ASCII_DELETE 127
 #define ASCII_BLOCK 219
+
+void resetStatistics();
+void computeStatistics();
+void printCLHelp(char *clarg0);
 
 #endif
